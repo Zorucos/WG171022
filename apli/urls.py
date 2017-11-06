a55635e4f0488546a1c9dc6bf22b8f17eb6da659
ismaelsoruco <30440769+ismaelsoruco@users.noreply.github.com>
2017-11-06 23:04:40 +0100

    nuevos avances

--- apli/urls.py
+++ apli/urls.py
@@ -3,26 +3,37 @@ from . import views
 
 
 urlpatterns = [
-	
-	#DASHBOARD
 
+   #ASSIGNMENT: index, detail, create, update, delete.
+
+    url(r'^assignment/$', views.assignment_index, name='assignment_index'),
+
+    url(r'^assignment/(?P<pk>[0-9]+)/$', views.assignment_detail, name='assignment_detail'),
+    url(r'^assignment/new/$', views.AssignmentCreate.as_view(), name='assignment_new'),
+    url(r'^assignment/(?P<pk>[0-9]+)/update/$', views.AssignmentUpdate.as_view(), name='assignment_update'),
+    url(r'^assignment/(?P<pk>[0-9]+)/delete/$', views.AssignmentDelete.as_view(), name='assignment_delete'),
+    #url(r'^assignment/(?P<pk>[0-9]+)/send/$', views.Assignment_send, name='assignment_send'),
+	
+	#DASHBOARD: index
 	url(r'^dashboard$', views.dashboard, name='dashboard'),
 
-	# PERSON: index, 
+	# PERSON: index, detail, create, update, delete.
 
 	url(r'^person$', views.person_index, name='person_index'),
-	    # /person/new/
+
 	url(r'^person/(?P<pk>[0-9]+)/$', views.person_detail, name='person_detail'),
-	    # /person/new/
     url(r'^person/new/$', views.PersonCreate.as_view(), name='person_new'),
-    # /person/<id>/update/
-    url(r'^person/(?P<pk>[0-9]+)/create/$', views.PersonCreate.as_view(), name='person_create'),
-    # /person/<id>/update/
     url(r'^person/(?P<pk>[0-9]+)/update/$', views.PersonUpdate.as_view(), name='person_update'),
-    # /person/<id>/delete/
     url(r'^person/(?P<pk>[0-9]+)/delete/$', views.PersonDelete.as_view(), name='person_delete'),
 
-    # PROJECT
+    # PROJECT: index, detail, create, update, delete.
+
     url(r'^project/$', views.project_index, name='project_index'),
+
     url(r'^project/(?P<pk>[0-9]+)/$', views.project_detail, name='project_detail'),
+    url(r'^project/new/$', views.ProjectCreate.as_view(), name='project_new'),
+    url(r'^project/(?P<pk>[0-9]+)/update/$', views.ProjectUpdate.as_view(), name='project_update'),
+    url(r'^project/(?P<pk>[0-9]+)/delete/$', views.ProjectDelete.as_view(), name='project_delete'),
+
+
 ]
--- apli/views.py
+++ apli/views.py
@@ -17,14 +17,68 @@ from django.core.mail import send_mail # send email
 from django.contrib import messages 
 from django.conf import settings # mail
 
+#--------------------------
+# INDICE (ordenar alfabéticamente)
+# 1- dashboard ...linea 28
+# 2- PERSON    ...linea 34
+# 3- Project   ...linea 107
+# -------------------------
 
-# DASHBOARD 
+
+@login_required(login_url='/register/login/')
+def assignment_index(request):
+    all_assignments = Assignment.objects.all().filter(sort='')
+    return render(request, 'apli/menu/assignment/assignments_index.html', {'all_assignments': all_assignments})
+
+@login_required(login_url='/register/login/')
+def assignment_detail(request, pk):
+    assignment = get_object_or_404(Assignment, id=pk)
+    all_horaire = assignment.horaire_set.all()
+    return render(request, 'apli/menu/assignment/assignment_detail.html', {'assignment': assignment, 'all_horaire': all_horaire})
+
+class AssignmentCreate(LoginRequiredMixin, CreateView):
+    model = Assignment
+    fields = ['project',
+            'person',
+            'model_type',
+            'travel_cost',
+            'hotel_cost',
+            'other_cost',
+            'comment_WG',
+            'statut',
+            'send_date',
+            'payment_date',
+            'total_price'
+            ]
+
+class AssignmentUpdate(LoginRequiredMixin, UpdateView):
+    model = Assignment
+    fields = ['project',
+            'person',
+            'model_type',
+            'travel_cost',
+            'hotel_cost',
+            'other_cost',
+            'comment_WG',
+            'statut',
+            'send_date',
+            'payment_date',
+            'total_price'
+            ]
+
+
+class AssignmentDelete(LoginRequiredMixin, DeleteView):
+    model = Assignment
+    success_url = reverse_lazy('assignment_index')
+
+
+# 1- DASHBOARD 
 
 @login_required(login_url='/register/login/')
 def dashboard(request):
     return render(request, 'apli/menu/dashboard/dashboard.html', )
 
-# PERSON: index, detail, create, update, delete.
+# 2- PERSON: index, detail, create, update, delete.
 
 @login_required(login_url='/register/login/')
 def person_index(request):
@@ -94,9 +148,11 @@ class PersonUpdate(LoginRequiredMixin, UpdateView):
 
 class PersonDelete(LoginRequiredMixin, DeleteView):
     model = Person
-    success_url = reverse_lazy('index_person')   
+    success_url = reverse_lazy('person_index')   
 
-#  PROJECT
+
+#  PROJECT: Index, detail, create, update, delete.
+
 @login_required(login_url='/register/login/')
 def project_index(request):
     all_projects = Project.objects.all()
@@ -109,3 +165,54 @@ def project_detail(request, pk):
     all_attachments = project.attachment_set.all()
     all_costs = project.cost_set.all()
     return render(request, 'apli/menu/project/project_detail.html', {'project': project, 'all_persons': all_persons, 'all_attachments': all_attachments, 'all_costs': all_costs})
+
+class ProjectCreate(LoginRequiredMixin, CreateView):
+    model = Project
+    fields = ['name',
+            'client',
+            'start',
+            'finish',
+            'user',
+            'comment',
+            'sort',
+            'all_day',
+            'half_day',
+            'half_day_price_pro',
+            'all_day_price_pro',
+            'over_price_pro',
+            'all_in_price_pro',
+            'half_day_price_semipro',
+            'all_day_price_semipro',
+            'over_price_semipro',
+            'all_in_price_semipro',
+            'country',
+            'city',
+            'zip_code',
+            'address',
+            'comment_address',
+            'honorary_base',
+            'honorary_plus',
+            'quantity_models_honorary_plus',
+            'ms_price',
+            'ms_hours',
+            'requirement_price',
+            'requirement_hours',
+            'requisiten_price_for_each_model',
+            'other_title',
+            'other_description',
+            'other_price',
+            'other_hours',
+            'photo_price',
+            'photo_hours',
+            'tax',
+            ]
+
+
+class ProjectUpdate(LoginRequiredMixin, UpdateView):
+    model = Project
+    fields = ['name', 'client', 'start', 'finish', 'user', 'comment', 'sort', 'all_day', 'half_day', 'half_day_price_pro', 'all_day_price_pro', 'over_price_pro', 'all_in_price_pro', 'half_day_price_semipro', 'all_day_price_semipro', 'over_price_semipro', 'all_in_price_semipro', 'country', 'city', 'zip_code', 'address', 'comment_address', 'honorary_base', 'honorary_plus', 'quantity_models_honorary_plus', 'ms_price', 'ms_hours', 'requirement_price', 'requirement_hours', 'requisiten_price_for_each_model', 'other_title', 'other_description', 'other_price', 'other_hours', 'photo_price', 'photo_hours', 'tax']
+
+
+class ProjectDelete(LoginRequiredMixin, DeleteView):
+    model = Project
+    success_url = reverse_lazy('project_index')
