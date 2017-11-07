574010c863ced49c705df8017b3912ed89f53e87
Ismael <30440769+ismaelsoruco@users.noreply.github.com>
2017-11-07 10:10:10 +0100

    Update views.py

--- register/views.py
+++ register/views.py
@@ -7,7 +7,13 @@ from .forms import RegisterForm, SignUpForm
 
 # from django.contrib.auth.forms import UserCreationForm
 # from django.contrib.auth import login, authenticate
-
+# from django.contrib.sites.shortcuts import get_current_site
+# from django.shortcuts import render, redirect
+# from django.utils.encoding import force_bytes
+# from django.utils.http import urlsafe_base64_encode
+# from django.template.loader import render_to_string
+# from mysite.core.forms import SignUpForm
+# from mysite.core.tokens import account_activation_token
 
 
 User = get_user_model()
@@ -24,12 +30,19 @@ class RegisterView(CreateView):
 #     if request.method == 'POST':
 #         form = SignUpForm(request.POST)
 #         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=raw_password)
-#             login(request, user)
-#             return redirect('home')
+#             user = form.save(commit=False)
+#             user.is_active = False
+#             user.save()
+#             current_site = get_current_site(request)
+#             subject = 'Activate Your MySite Account'
+#             message = render_to_string('account_activation_email.html', {
+#                 'user': user,
+#                 'domain': current_site.domain,
+#                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+#                 'token': account_activation_token.make_token(user),
+#             })
+#             user.email_user(subject, message)
+#             return redirect('account_activation_sent')
 #     else:
 #         form = SignUpForm()
 #     return render(request, 'signup.html', {'form': form})
