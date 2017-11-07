8a8816f4b36c2b46cd15a7414fab5e11580c6984
Ismael <30440769+ismaelsoruco@users.noreply.github.com>
2017-11-07 10:15:52 +0100

    activate email

--- register/views.py
+++ register/views.py
@@ -8,12 +8,14 @@ from .forms import RegisterForm, SignUpForm
 # from django.contrib.auth.forms import UserCreationForm
 # from django.contrib.auth import login, authenticate
 # from django.contrib.sites.shortcuts import get_current_site
-# from django.shortcuts import render, redirect
 # from django.utils.encoding import force_bytes
 # from django.utils.http import urlsafe_base64_encode
 # from django.template.loader import render_to_string
-# from mysite.core.forms import SignUpForm
-# from mysite.core.tokens import account_activation_token
+# from wg.register.forms import SignUpForm
+# from wg.register.tokens import account_activation_token
+# from django.contrib.auth.models import User
+# from django.utils.encoding import force_text
+# from django.utils.http import urlsafe_base64_decode
 
 
 User = get_user_model()
@@ -46,3 +48,25 @@ class RegisterView(CreateView):
 #     else:
 #         form = SignUpForm()
 #     return render(request, 'signup.html', {'form': form})
+
+# def activate(request, uidb64, token):
+#     try:
+#         uid = force_text(urlsafe_base64_decode(uidb64))
+#         user = User.objects.get(pk=uid)
+#     except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+#         user = None
+
+#     if user is not None and account_activation_token.check_token(user, token):
+#         user.is_active = True
+#         user.profile.email_confirmed = True
+#         user.save()
+#         login(request, user)
+#         return redirect('home')
+#     else:
+#         return render(request, 'account_activation_invalid.html')
+
+
+
+
+
+
