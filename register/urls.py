77e3b475c57d8e20df4f65645845722b9cc131b4
Ismael <30440769+ismaelsoruco@users.noreply.github.com>
2017-11-07 10:12:27 +0100

    Update urls.py

--- register/urls.py
+++ register/urls.py
@@ -19,6 +19,9 @@ urlpatterns = [
 	# REGISTER
 	url(r'^register/$', RegisterView.as_view(template_name='register.html'), name="register"),
 # 	url(r'^signup/$', register_views.signup, name='signup'),
+# 	url(r'^account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
+# 	url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+#         core_views.activate, name='activate'),
 
 
 	# RESET PASSWORD
