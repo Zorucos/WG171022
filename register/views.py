73a46f5af7febae9ce1882a2463a501122515e8f
Ismael <30440769+ismaelsoruco@users.noreply.github.com>
2017-11-07 10:00:21 +0100

    views.py

--- register/views.py
+++ register/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View, CreateView
 
 from django.views.generic import CreateView # registration
 from django.contrib.auth import get_user_model #registration
 from .forms import RegisterForm
 
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth import login, authenticate
 
 
 
@@ -15,3 +17,17 @@ class RegisterView(CreateView):
     template_name = 'register.html'
     success_url = "/register/login"
 
+    
+#     def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('home')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'signup.html', {'form': form})
