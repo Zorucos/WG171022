8a2514964e3ef590083b56220b4a3011e3d7bfc7
ismaelsoruco <30440769+ismaelsoruco@users.noreply.github.com>
2017-11-25 21:40:04 +0100

    sdf

--- apli/tables.py
+++ apli/tables.py
@@ -2,11 +2,15 @@ import django_tables2 as tables
 from .models import Person
 
 class PersonTable(tables.Table):#name
-	name = tables.TemplateColumn('<a href="{% url \'person_detail\' record.id %}"></a>')
+	Edit = tables.TemplateColumn('<a href="{% url \'person_detail\' record.id %}">Edit</a>', orderable=False)
+	phone = tables.Column(orderable=False)
+	selection = tables.CheckBoxColumn(accessor='pk', orderable=False)
+	
 
 	class Meta:
 		model = Person
 		email = tables.EmailColumn()
+
 
 		template = 'django_tables2/bootstrap.html'
 		fields = [
